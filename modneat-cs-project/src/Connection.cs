282204aa9cebb37b44142d97abe7c9680b06989b
katomasahiro10 <katomasahiro10@gmail.com>
2020-12-11 23:09:13 +0900

    Create class file

---
+++ modneat-cs-project/src/Connection.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modneat_cs_project
+{
+    class Connection
+    {
+        public readonly int connection_id;
+        public bool is_valid;
+        public readonly int input_id;
+        public readonly int output_id;
+
+        public float weight;
+        public readonly float initial_weight;
+
+
+        public Connection(int connection_id, int input_id, int output_id, int seed)
+        {
+            this.connection_id = connection_id;
+            this.is_valid = true;
+            this.input_id = input_id;
+            this.output_id = output_id;
+
+            var random = new System.Random(seed);
+            this.weight = ((float)random.Next(-100_000, 100_000) / 100_100.0f);
+            this.initial_weight = this.weight;
+        }
+    }
+}
