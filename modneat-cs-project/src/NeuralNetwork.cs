2613266215cad2b96540a8cd5f31862fb35522f6
katomasahiro10 <katomasahiro10@gmail.com>
2020-12-29 10:56:46 +0900

    NeuralNetworkクラス 作成中

---
+++ modneat-cs-project/src/NeuralNetwork.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modneat_cs_project
+{
+    class NeuralNetwork
+    {
+        List<Neuron> neurons = new List<Neuron>();
+        List<Connection> connections = new List<Connection>();
+    }
+}
